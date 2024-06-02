22b7aca8c8f39f6d652156f8238d0103685a00dc
Akash Singh <mr.singhaksh@gmail.com>
2024-06-02 23:51:30 +0530

    fix:resolve import issue

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,9 +1,13 @@
 // src/pages/dashboard.js
 import React from 'react';
-import PrivateRoute from '../components/PrivateRoute';
-import Header from '../components/Header';
-import LandingPageList from '../components/LandingPageList';
-import Footer from '../components/Footer';
+// import PrivateRoute from '../components/PrivateRoute';
+import PrivateRoute from '@/component/PrivateRoute';
+// import Header from '../components/Header';
+import Header from '@/component/Header';
+// import LandingPageList from '../components/LandingPageList';
+import LandingPageList from '@/component/LandingPageList';
+// import Footer from '../components/Footer';
+import Footer from "@/component/Footer"
 
 const Dashboard = () => {
   return (
